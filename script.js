9d65c31938e4c5a467331a622d2b32ae44bb49c5
gaurav700 <jangidg0786@gmail.com>
2025-01-23 09:26:37 -0500

    creating the workout , running, cycling classes for storing data

--- script.js
+++ script.js
@@ -7,3 +7,53 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+class Workout{
+  date = new Date();
+  id = Date.now();
+  constructor(coords, distance, duration){
+    // this.date = ...
+    // this.id = ...
+    this.coords = coords;
+    this.distance = distance; // in km
+    this.duration = duration; // in  min
+  }
+
+  _setDiscription(){
+    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+    this.description= `${this.type[0].toUpperCase()}${this.type.slice(1)} on ${months[this.date.getMonth()]} ${this.date.getDate()}`;
+  }
+}
+
+class Running extends Workout{
+  type='running';
+  constructor(coords, distance, duration, cadence){
+    super(coords, distance,duration);
+    this.cadence = cadence;
+    this.type='running';
+    this.calcPace();
+    this._setDiscription();
+  }
+  calcPace(){
+    // min per miles
+    this.pace = this.duration / this.distance*1.6;
+    return this.pace;
+  }
+}
+
+class Cycling extends Workout{
+  type='cycling';
+  constructor(coords, distance, duration, elevGain){
+    super(coords, distance,duration);
+    this.elevGain = elevGain;
+    this.type = 'cycling';
+    this.calcSpeed();
+    this._setDiscription();
+  }
+  calcSpeed(){
+    // miles per hr
+    this.speed = this.distance*1.6 / this.duration/60;
+    return this.speed;
+  }
+}
